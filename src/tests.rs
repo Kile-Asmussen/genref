b7883713063d1b6d75b196d278f3b432e00fa751
Kile Kasmir Asmussen <kile.asmussen@gmail.com>
2022-05-12 01:27:12 +0200

    better tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,42 +1,35 @@
 use std::cell::Cell;
 
-use crate::genref::{allocator, generations::InUsePtr, pointers::Owned};
+use crate::allocator::*;
+use crate::generations::*;
+use crate::pointers::*;
 
 #[test]
-fn generation_marker()
+fn user_story()
 {
-    let alloc = InUsePtr::allocate(0);
-    let gen1 = alloc.generation();
-    unsafe {
-        alloc.upcast().unwrap().downcast::<i32>(2);
-    }
-    let gen2 = alloc.generation();
-    assert_eq!(gen1 + 2, gen2);
-}
-
-#[test]
-fn allocation()
-{
-    assert_eq!(allocator::get_stats().guards, 0);
+    assert_eq!(thread_local_stats().guards, 0);
 
     let x = Owned::new(Cell::new(2i32));
 
     let y = x.alias();
 
     let z = y.try_ref();
+
     assert!(z.is_some());
+
     let z = z.unwrap();
 
-    assert_eq!(allocator::get_stats().guards, 1);
+    assert_eq!(thread_local_stats().guards, 1);
 
     assert_eq!(z.get(), 2);
 
     z.set(3i32);
 
     let q = x.alias();
+
     assert_eq!(q.try_ref().map(|z| z.get()), Some(3));
 
-    let x = match x.try_take() {
+    let x = match x.try_into_inner() {
         Ok(_) => {
             assert!(false, "impossible");
             return;
@@ -45,9 +38,10 @@ fn allocation()
     };
 
     std::mem::drop(z);
-    assert_eq!(allocator::get_stats().guards, 0);
 
-    let _ = match x.try_take() {
+    assert_eq!(thread_local_stats().guards, 0);
+
+    let _ = match x.try_into_inner() {
         Ok(i) => i,
         Err(_) => {
             assert!(false, "impossible");
@@ -57,5 +51,42 @@ fn allocation()
 
     assert!(y.try_ref().is_none());
 
-    assert!(!allocator::get_stats().by_layout.is_empty());
+    assert!(!thread_local_stats().by_layout.is_empty());
+}
+
+#[test]
+fn stress_test()
+{
+    let n = 500;
+    for _ in 0..n {
+        let mut x = Uniq::<Vec<Owned<i32>>>::new(Vec::<Owned<i32>>::new());
+
+        for j in 0..n {
+            x.push(Owned::new(j));
+        }
+    }
+    assert_eq!(thread_local_stats().free_objects(), n as usize + 1);
+}
+
+#[test]
+fn stress_test2()
+{
+    let n = 10;
+    for _ in 0..n {
+        let mut x = Uniq::<Vec<Uniq<Vec<Uniq<i32>>>>>::new(Vec::new());
+
+        for _ in 0..n {
+            let mut y = Uniq::new(Vec::new());
+
+            for j in 0..n {
+                y.push(Uniq::new(j));
+            }
+
+            x.push(y);
+        }
+    }
+    assert_eq!(
+        thread_local_stats().free_objects(),
+        (n * n + n + 1) as usize
+    );
 }
