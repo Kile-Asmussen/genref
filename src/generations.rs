4b7eca3540291037e4be0346377d07c30ddc1478
Kile Kasmir Asmussen <kile.asmussen@gmail.com>
2022-05-12 01:26:41 +0200

    more granular api + debug instrumentation

--- src/generations.rs
+++ src/generations.rs
@@ -1,5 +1,7 @@
 use std::{
     alloc::Layout,
+    // any::type_name,
+    fmt,
     hash::{self, Hasher},
     mem::MaybeUninit,
     ptr::NonNull,
@@ -23,10 +25,12 @@ impl<T: 'static> Generation<T>
 
     fn generation(&self) -> usize { self.gen.load(Ordering::Relaxed) }
 
-    fn bump_generation(&self) -> bool { self.gen.fetch_add(1, Ordering::Relaxed) != usize::MAX }
+    fn bump_generation(&self) { self.gen.fetch_add(1, Ordering::Relaxed); }
+
+    fn is_end_of_life(&self) -> bool { self.generation() == usize::MAX }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy)]
 pub(crate) struct FreePtr(pub(crate) NonNull<Generation<()>>);
 
 impl FreePtr
@@ -42,46 +46,65 @@ impl FreePtr
 
 unsafe impl Send for FreePtr {}
 
-#[derive(Debug)]
+impl fmt::Debug for FreePtr
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result
+    {
+        f.debug_tuple("FreePtr").field(&self.0).finish()
+    }
+}
+
 pub(crate) struct InUsePtr<T: 'static>(pub(crate) NonNull<Generation<T>>);
+
 impl<T: 'static> Clone for InUsePtr<T>
 {
     fn clone(&self) -> Self { InUsePtr(self.0) }
 }
 impl<T: 'static> Copy for InUsePtr<T> {}
 
+impl<T: 'static> fmt::Debug for InUsePtr<T>
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result
+    {
+        f.debug_tuple("InUsePtr").field(&self.0).finish()
+    }
+}
+
 impl<T: 'static> InUsePtr<T>
 {
     pub(crate) fn allocate(data: T) -> InUsePtr<T>
     {
+        //dbg!(
+        //    "InUsePtr::<{1}>::allocate(_) => {0:?}",
         Self(unsafe {
             NonNull::new_unchecked(Box::into_raw(Box::new(Generation {
                 gen: AtomicUsize::new(1),
                 data: MaybeUninit::new(data),
             })))
         })
+        //,type_name::<T>() )
     }
 
     pub(crate) unsafe fn upcast(self) -> Option<FreePtr>
     {
         let mut ptr = self.0;
         let alloc = ptr.as_mut();
-        let res = if alloc.bump_generation() {
-            Some(FreePtr(ptr.cast()))
-        } else {
+        let res = if alloc.is_end_of_life() {
             None
+        } else {
+            Some(FreePtr(ptr.cast()))
         };
         alloc.drop_data();
         res
     }
 
-    pub(crate) fn invalidate_weak(&self) -> bool { unsafe { self.0.as_ref().bump_generation() } }
+    pub(crate) fn invalidate_weaks(&self) { unsafe { self.0.as_ref().bump_generation() } }
 
     pub(crate) unsafe fn upcast_take(self) -> (T, Option<FreePtr>)
     {
         let mut ptr = self.0;
         let alloc = ptr.as_mut();
-        let res = if alloc.bump_generation() {
+        let res = if alloc.is_end_of_life() {
             Some(FreePtr(ptr.cast()))
         } else {
             None
@@ -111,6 +134,12 @@ impl GenerationLayout
 {
     /// Produces the layout of an generational allocation of `T`.
     pub fn of<T: 'static>() -> Self { GenerationLayout(Layout::new::<Generation<T>>()) }
+
+    /// Delegates to underlying `Layout`
+    pub fn size(&self) -> usize { self.0.size() }
+
+    /// Delegates to underlying `Layout`
+    pub fn align(&self) -> usize { self.0.align() }
 }
 
 impl hash::Hash for GenerationLayout
@@ -125,4 +154,15 @@ impl hash::Hash for GenerationLayout
 impl From<GenerationLayout> for Layout
 {
     fn from(it: GenerationLayout) -> Self { it.0 }
+}
+
+impl fmt::Debug for GenerationLayout
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result
+    {
+        f.debug_struct("GenerationLayout")
+            .field("size()", &self.size())
+            .field("align()", &self.align())
+            .finish()
+    }
 }
