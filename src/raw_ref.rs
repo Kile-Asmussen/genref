6ab12c50e864e96ec002d002b8d8e238bf1a1652
Kile K Asmussen <kile.asmussen@gmail.com>
2023-07-08 21:47:31 +0200

    .

--- src/raw_ref.rs
+++ src/raw_ref.rs
@@ -1,108 +1,58 @@
-use std::{mem, ptr::NonNull};
+use std::{mem, num::NonZeroU64, ptr::NonNull};
+
+use crate::tracking::{self, Account, AccountEnum};
 
 use super::{
     global_ledger::GlobalIndex,
-    local_ledger::{self, LocalIndex},
+    local_ledger::{self},
+    tracking::*,
 };
 
-pub(crate) trait Tracking
+pub(crate) enum PointerEnum<T>
 {
-    fn generation(&self) -> u64;
-    fn invalidate(&self) -> u64;
-    fn try_lock_exclusive(&self) -> bool;
-    fn lock_exclusive(&self);
-    fn try_lock_shared(&self) -> bool;
-    fn try_upgrade(&self) -> bool;
-    unsafe fn unlock_exclusive(&self);
-    unsafe fn unlock_shared(&self);
+    Weak(NonNull<T>),
+    Strong(NonNull<T>),
 }
 
-#[derive(Clone, Copy)]
-union Account
+impl<T> Clone for PointerEnum<T>
 {
-    local: LocalIndex,
-    global: GlobalIndex,
+    fn clone(&self) -> Self
+    {
+        match self {
+            Self::Weak(arg0) => Self::Weak(arg0.clone()),
+            Self::Strong(arg0) => Self::Strong(arg0.clone()),
+        }
+    }
 }
 
-#[derive(Clone, Copy)]
-pub(crate) enum AccountEnum
+impl<T> std::fmt::Debug for PointerEnum<T>
 {
-    Nil,
-    Local(LocalIndex),
-    Global(GlobalIndex),
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    {
+        match self {
+            Self::Weak(arg0) => f.debug_tuple("Weak").field(arg0).finish(),
+            Self::Strong(arg0) => f.debug_tuple("Strong").field(arg0).finish(),
+        }
+    }
 }
 
-impl Tracking for AccountEnum
+impl<T> PointerEnum<T>
 {
-    fn generation(&self) -> u64
+    pub(crate) fn as_ptr(self) -> NonNull<T>
     {
         match self {
-            Nil => 0,
-            Self::Local(l) => l.generation(),
-            Self::Global(g) => g.generation(),
+            PointerEnum::Weak(p) => p,
+            PointerEnum::Strong(p) => p,
         }
     }
 
-    fn invalidate(&self) -> u64
+    pub(crate) fn map<F, U>(self, f: F) -> PointerEnum<U>
+    where
+        F: FnOnce(NonNull<T>) -> NonNull<U>,
     {
         match self {
-            Nil => 0,
-            Self::Local(l) => l.invalidate(),
-            Self::Global(g) => g.invalidate(),
-        }
-    }
-
-    fn try_lock_exclusive(&self) -> bool
-    {
-        match self {
-            Nil => false,
-            Self::Local(l) => l.try_lock_exclusive(),
-            Self::Global(g) => g.try_lock_exclusive(),
-        }
-    }
-
-    fn lock_exclusive(&self)
-    {
-        match self {
-            Nil => (),
-            Self::Local(l) => l.lock_exclusive(),
-            Self::Global(l) => l.lock_exclusive(),
-        }
-    }
-
-    fn try_lock_shared(&self) -> bool
-    {
-        match self {
-            Nil => false,
-            Self::Local(l) => l.try_lock_shared(),
-            Self::Global(g) => g.try_lock_shared(),
-        }
-    }
-
-    fn try_upgrade(&self) -> bool
-    {
-        match self {
-            Nil => false,
-            Self::Local(l) => l.try_upgrade(),
-            Self::Global(g) => g.try_upgrade(),
-        }
-    }
-
-    unsafe fn unlock_exclusive(&self)
-    {
-        match self {
-            Nil => (),
-            Self::Local(l) => l.unlock_exclusive(),
-            Self::Global(g) => g.unlock_exclusive(),
-        }
-    }
-
-    unsafe fn unlock_shared(&self)
-    {
-        match self {
-            Nil => (),
-            Self::Local(l) => l.unlock_shared(),
-            Self::Global(g) => g.unlock_shared(),
+            Self::Weak(p) => PointerEnum::Weak(f(p)),
+            Self::Strong(p) => PointerEnum::Strong(f(p)),
         }
     }
 }
@@ -110,9 +60,9 @@ impl Tracking for AccountEnum
 #[repr(C)]
 pub(crate) struct RawRef<T>
 {
-    account: Option<Account>,
-    pointer: Option<NonNull<T>>,
-    generation: u64,
+    account: Account,
+    pointer: NonNull<T>,
+    generation: NonZeroU64,
 }
 
 impl<T> Clone for RawRef<T>
@@ -126,156 +76,105 @@ impl<T> Clone for RawRef<T>
         }
     }
 }
-impl<T> Copy for RawRef<T> {}
-
-pub(crate) enum PointerEnum<T>
-{
-    Nil,
-    Weak(NonNull<T>),
-    Strong(NonNull<T>),
-}
 
 impl<T> RawRef<T>
 {
-    fn nil() -> Self
-    {
-        RawRef {
-            account: None,
-            pointer: None,
-            generation: 0,
-        }
-    }
-
-    fn is_nil(self) -> bool
-    {
-        self.generation == 0 && self.account.is_none() && self.pointer.is_none()
-    }
-
-    pub(crate) fn is_non_nil(self) -> bool
-    {
-        self.generation != 0 && self.account.is_some() && self.pointer.is_some()
-    }
-
     #[cfg(test)]
-    fn invariant(self) -> Self
+    pub(crate) fn invariant(&self)
     {
-        if self.generation == 0 {
-            assert!(
-                self.account.is_none(),
-                "zero generation on reference with non-nil account"
-            );
-            assert!(
-                self.pointer.is_none(),
-                "zero generation on reference with non-nil pointer"
-            );
-            return self;
-        }
+        let reference = self.generation.get() & Self::REFERENCE_MASK;
+        let account = self.generation.get() & Self::ACCOUNT_MASK;
+        let counter = self.generation.get() & Self::COUNTER_MASK;
 
-        let reference = self.generation & Self::REFERENCE_MASK;
-        let account = self.generation & Self::ACCOUNT_MASK;
-        let counter = self.generation & Self::COUNTER_MASK;
-
-        assert!(counter != 0, "flags set on nil generation count");
-        assert!(account != 0, "no account flag on positive generation count");
-        assert!(account != Self::ACCOUNT_MASK, "saturated account flags");
-        assert!(
-            reference != 0,
-            "no reference flag on positive generation count"
-        );
-        assert!(
-            reference != Self::REFERENCE_MASK,
-            "saturated reference flags"
-        );
-        assert!(
-            self.account.is_some(),
-            "nonzero generation on reference with nil account"
-        );
-        assert!(
-            self.pointer.is_some(),
-            "nonzero generation on reference with nil pointer"
-        );
-        self
+        assert_ne!(counter, 0, "flags set on nil generation count");
+        assert_ne!(account, 0, "no account flag on positive generation count");
+        assert_ne!(account, Self::ACCOUNT_MASK, "saturated account flags");
+        assert_ne!(reference, 0, "no reference flag");
+        assert_ne!(reference, Self::REFERENCE_MASK, "saturated reference flags");
     }
 
     #[cfg(not(test))]
-    fn invariant(self) -> Self { self }
+    pub(crate) fn invariant(&self) {}
 
     fn new_from_parts(acc: AccountEnum, ptr: PointerEnum<T>) -> Self
     {
         let (account, acc_flag) = match acc {
-            AccountEnum::Nil => (None, 0),
-            AccountEnum::Local(local) => (Some(Account { local }), Self::LOCAL_ACCOUNT),
-            AccountEnum::Global(global) => (Some(Account { global }), Self::GLOBAL_ACCOUNT),
+            AccountEnum::Local(local) => (tracking::Account { local }, Self::LOCAL_ACCOUNT),
+            AccountEnum::Global(global) => (tracking::Account { global }, Self::GLOBAL_ACCOUNT),
         };
         let (pointer, ref_flag) = match ptr {
-            PointerEnum::Nil => (None, 0),
-            PointerEnum::Weak(p) => (Some(p), Self::WEAK_REFERENCE),
-            PointerEnum::Strong(p) => (Some(p), Self::STRONG_REFERENCE),
+            PointerEnum::Weak(p) => (p, Self::WEAK_REFERENCE),
+            PointerEnum::Strong(p) => (p, Self::STRONG_REFERENCE),
         };
-        let generation = acc.generation() | acc_flag | ref_flag;
+        let generation = NonZeroU64::new(acc.generation() | acc_flag | ref_flag).unwrap();
         let res = RawRef {
             account,
             pointer,
             generation,
         };
-        res.invariant()
+        res.invariant();
+        res
     }
 
-    pub(crate) fn new_from_box(mut it: Box<T>) -> Self
+    pub(crate) fn from_box(mut it: Box<T>) -> Self
     {
         let res = Self::new_from_parts(
             AccountEnum::Local(local_ledger::allocate()),
             PointerEnum::Strong(NonNull::from(it.as_mut())),
         );
         mem::forget(it);
-        res.invariant()
+        res.invariant();
+        res
     }
 
-    pub(crate) fn account(self) -> AccountEnum
+    pub(crate) fn map<F, U>(self, f: F) -> RawRef<U>
+    where
+        F: FnOnce(NonNull<T>) -> NonNull<U>,
+    {
+        RawRef::new_from_parts(self.account(), self.pointer().map(f))
+    }
+
+    pub(crate) fn account(&self) -> AccountEnum
     {
         self.invariant();
-        if let Some(a) = self.account {
-            match self.generation & Self::ACCOUNT_MASK {
-                GLOBAL_ACCOUNT => AccountEnum::Global(unsafe { a.global }),
-                LOCAL_ACCOUNT => AccountEnum::Local(unsafe { a.local }),
-                _ => panic!(),
-            }
-        } else {
-            AccountEnum::Nil
+        match self.generation.get() & Self::ACCOUNT_MASK {
+            Self::GLOBAL_ACCOUNT => AccountEnum::Global(unsafe { self.account.global }),
+            Self::LOCAL_ACCOUNT => AccountEnum::Local(unsafe { self.account.local }),
+            _ => panic!(),
         }
     }
 
-    pub(crate) fn pointer(self) -> PointerEnum<T>
+    pub(crate) fn pointer(&self) -> PointerEnum<T>
     {
-        if let Some(p) = self.invariant().pointer {
-            match self.generation & Self::REFERENCE_MASK {
-                STRONG_REFERENCE => PointerEnum::Strong(p),
-                WEAK_REFERENCE => PointerEnum::Weak(p),
-                _ => panic!(),
-            }
-        } else {
-            PointerEnum::Nil
+        self.invariant();
+        match self.generation.get() & Self::REFERENCE_MASK {
+            Self::STRONG_REFERENCE => PointerEnum::Strong(self.pointer),
+            Self::WEAK_REFERENCE => PointerEnum::Weak(self.pointer),
+            _ => panic!(),
         }
     }
 
-    pub(crate) fn as_weak(mut self) -> Self
+    pub(crate) fn set_weak(mut self) -> Self
     {
         self.invariant();
-        self.generation &= !Self::REFERENCE_MASK;
-        self.generation |= Self::WEAK_REFERENCE;
+        self.generation =
+            NonZeroU64::new((self.generation.get() & !Self::REFERENCE_MASK) | Self::WEAK_REFERENCE)
+                .unwrap();
         self.invariant();
         self
     }
 
-    fn as_global(mut self) -> Self
+    fn set_global(mut self) -> Self
     {
         self.invariant();
-        self.generation &= !Self::ACCOUNT_MASK;
-        self.generation |= Self::GLOBAL_ACCOUNT;
-        self.invariant()
+        self.generation =
+            NonZeroU64::new((self.generation.get() & !Self::ACCOUNT_MASK) | Self::GLOBAL_ACCOUNT)
+                .unwrap();
+        self.invariant();
+        self
     }
 
-    fn counter(self) -> u64 { self.generation & Self::COUNTER_MASK }
+    fn counter(self) -> u64 { self.generation.get() & Self::COUNTER_MASK }
 
     const FLAG_MASK: u64 = 0b1111u64.reverse_bits();
     pub(crate) const COUNTER_MASK: u64 = !Self::FLAG_MASK;
